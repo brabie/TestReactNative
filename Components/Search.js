33b6d159137fd8c236ddf3ccc002836d864af2de
Essrifi Ayoub <Ayoub.Essrifi@sade-cgth.fr>
2020-02-25 16:25:26 +0100

    adding FilmItem component and Flatlist to Search component

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { StyleSheet, Button, TextInput, View} from 'react-native';
+import { StyleSheet, Button, TextInput, View, Text, FlatList} from 'react-native';
+import films from '../Helpers/filmsData';
+import FilmItem from './FilmItem'
 
 export default function Search() {
   return (
-    <View>
-      <TextInput style={styles.textinput} placeholder="Entrer le titre du film" />
-      <Button title="Rechercher" onPress={ () => {} } />
+    <View style={styles.main_container}>
+      <TextInput style = {styles.textinput} placeholder = "Entrer le titre du film" />
+      <Button title = "Rechercher" onPress = { () => {} } />
+      <FlatList
+      data = {films}
+      keyExtractor = {(item) => {item.id.toString()}}
+      renderItem = {({item}) => <FilmItem/>}
+      />
     </View>
   );
 }
 const styles = StyleSheet.create({
+  main_container:{
+    marginTop: 20,
+    flex: 1
+  },
   textinput: {
     marginLeft: 5,
     marginRight: 5,
